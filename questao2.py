a2a5907a2722a2f5049adc75a6766c9f864100c0
Jose <jose8.trabalho@protonmail.com>
2022-11-19 20:54:27 -0300

    alteração valores questao 2

--- questao2.py
+++ questao2.py
@@ -2,7 +2,7 @@
 # Se o salário for menor que 1000 reais, forneça um aumento de 10% e sobrescreva o valor antigo. 
 # Ao final, mostre a lista de salários atualizada.
 
-vetor = [909, 2000, 780, 700, 1500]
+vetor = [929, 2040, 780, 700, 1500]
 
 new_vetor = [round(x*1.1, 2) if x<1000 else x for x in vetor]
 
